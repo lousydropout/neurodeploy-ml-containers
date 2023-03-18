88d780313e2ee1f0bbb5fe8c33257b8c93c650f0
soraya <soraya.bha@gmail.com>
2023-03-17 17:21:53 -0700

    Add scikit learn

--- ml-cache/main.py
+++
@@ -1,9 +0,0 @@
-import json
-import tensorflow as tf
-
-
-def handler(event: dict, context):
-    print("Event: ", json.dumps(event, default=str))
-    print(tf.reduce_sum(tf.random.normal([1000, 1000])))
-    print("Tensorflow version: ", tf.__version__)
-    return
